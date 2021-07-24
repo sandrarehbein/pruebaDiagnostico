877898c2ce6bda15495987126416fe34536ac4f3
fanny <sandra.rehbein@gmail.com>
2021-07-23 21:07:03 -0400

    suma de numeros impares

--- prueba.js
+++ prueba.js
@@ -1,9 +1,10 @@
 var n =parseInt(prompt("Ingresa un numero"));
-
+var suma=0;
 for(let i=1; i<=n;i++){
-if(i%2==0){
+if(i%2!=0){
     console.log(i);
+    suma=suma+i;
 }
 
 
-}
+}console.log("la suma es "+ suma);
